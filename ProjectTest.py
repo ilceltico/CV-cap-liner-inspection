b25e206379b69ce9f616638979007179b16b138d
dome <federicodomeniconi@gmail.com>
2019-01-24 15:36:17 +0100

    Little fix of bug that caused crash where no circles were found

--- ProjectTest.py
+++ ProjectTest.py
@@ -5,7 +5,7 @@ import os
 
 # hough trasform
 dp = 1
-minDist = 20
+minDist = 5
 
 # WARNING: last 4 parameters must be inserted below by hand
 p1 = 0
@@ -34,8 +34,8 @@ sigmaY = 0
 
 bilateral = True
 diameter = -1
-sigmaColor = 1
-sigmaSpace = 1
+sigmaColor = 10
+sigmaSpace = 10
 
 # they say it's for sharpness enhancement and noise removal
 # WARNING: not working because it disappeared
@@ -48,7 +48,6 @@ aMaxSigmaColor = 0
 
 for file in os.listdir('./caps'):
     img = cv2.imread('caps/' + file, cv2.IMREAD_GRAYSCALE)
-    cv2.imshow('original', img)
     
     if gamma:
         table = np.array([255 ** (1 - r) * i ** r for i in np.arange(0, 256)]).astype('uint8')
@@ -67,14 +66,15 @@ for file in os.listdir('./caps'):
 
     cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
 
-    #circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, dp, minDist)
+    circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, dp, minDist)
 
-    #circles = np.uint16(np.around(circles))
-    #for i in circles[0,:]:
-    #    # draw the outer circle
-    #    cv2.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 1)
-    #    # draw the center of the circle
-    #    cv2.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        for i in circles[0,:]:
+            # draw the outer circle
+            cv2.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 1)
+            # draw the center of the circle
+            cv2.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
 
     cv2.imshow('detected circles', cimg)
     cv2.waitKey()
