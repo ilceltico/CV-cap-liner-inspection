cc49f1f9da965f1f583d66b6382d91564b811f18
Andrea Gavagna <andrea.gavagna@studio.unibo.it>
2019-02-12 18:21:41 +0100

    Implemented pixelAverage and pixelAverageMask(mask) to detect the cap without liner (the image with a significant difference in average, average2 or sum from the other images).

--- linerdefects_gradient.py
+++ linerdefects_gradient.py
@@ -34,7 +34,72 @@ def calcMagnitude(img):
 #    res = Point.is_collinear(*points)
 #    return res
 
-@profile
+#@profile
+def pixelAverage():
+    for file in os.listdir('./caps'):
+        #t1 = cv2.getTickCount()
+
+        img = cv2.imread('caps/' + file, cv2.IMREAD_GRAYSCALE)
+        magnitude = calcMagnitude(img)
+        sum = cv2.sumElems(magnitude)
+        num = cv2.countNonZero(magnitude)
+        average = np.mean(magnitude)
+        average2 = sum[0]/num
+        #t2 = cv2.getTickCount()
+        #time = (t2 - t1)/ cv2.getTickFrequency()
+
+        cv2.imshow("caps/" + file, magnitude)
+        print("caps/" + file + " pixel's average: " + str(average))
+        print("caps/" + file + " pixel's non zero sum: " + str(sum))
+        print("caps/" + file + " pixel's non zero count: " + str(num))
+        print("caps/" + file + " pixel's average2: " + str(average2))
+        #time = (t2 - t1)/ cv2.getTickFrequency()
+        #print ('time: ' + str(time))
+        
+        print ('----------------------------------------')
+        cv2.waitKey()
+        cv2.destroyAllWindows()
+
+#@profile
+def pixelAverageMask(mask):
+    for file in os.listdir('./caps'):
+        #t1 = cv2.getTickCount()
+
+        img = cv2.imread('caps/' + file, cv2.IMREAD_GRAYSCALE)
+        magnitude = calcMagnitude(img)
+        sum = cv2.sumElems(magnitude[mask])
+        num = cv2.countNonZero(magnitude[mask])
+        average = np.mean(magnitude[mask])
+        average2 = sum[0]/num
+        #t2 = cv2.getTickCount()
+        #time = (t2 - t1)/ cv2.getTickFrequency()
+
+        cv2.imshow("caps/" + file, magnitude)
+        print("caps/" + file + " pixel's average: " + str(average))
+        print("caps/" + file + " pixel's non zero sum: " + str(sum))
+        print("caps/" + file + " pixel's non zero count: " + str(num))
+        print("caps/" + file + " pixel's average2: " + str(average2))
+        #time = (t2 - t1)/ cv2.getTickFrequency()
+        #print ('time: ' + str(time))
+        
+        print ('----------------------------------------')
+        cv2.waitKey()
+        cv2.destroyAllWindows()
+
+def circularmask(h, w, center=None, radius=None):
+
+    if center is None: # use the middle of the image
+        center = [int(w/2), int(h/2)]
+    if radius is None: # use the smallest distance between the center and image walls
+        radius = min(center[0], center[1], w-center[0], h-center[1])
+
+    Y, X = np.ogrid[:h, :w]
+    dist_from_center = np.sqrt((X - center[0])**2 + (Y-center[1])**2)
+
+    mask = dist_from_center <= radius
+    return mask
+
+#@profile
 def test():
     goodCap = cv2.imread("./caps/g_04.bmp", cv2.IMREAD_GRAYSCALE)
     incompleteLiner = cv2.imread("./caps/d_18.bmp", cv2.IMREAD_GRAYSCALE)
@@ -43,15 +108,19 @@ def test():
     incompleteLinerMagnitude = calcMagnitude(incompleteLiner)
     missingLinerMagnitude = calcMagnitude(missingLiner)
 
-    #cv2.imshow("Good Cap Magnitude", goodCapMagnitude)
-    #cv2.imshow("Incomplete Liner Magnitude", incompleteLinerMagnitude)
-    #cv2.imshow("Missing Liner Magnitude", missingLinerMagnitude)
+    cv2.imshow("Good Cap Magnitude", goodCapMagnitude)
+    cv2.imshow("Incomplete Liner Magnitude", incompleteLinerMagnitude)
+    cv2.imshow("Missing Liner Magnitude", missingLinerMagnitude)
+
+    print("Good Cap Magnitude average of pixels: " + str(np.mean(goodCapMagnitude)))
+    print("Incomplete Liner Magnitude average of pixels: " + str(np.mean(incompleteLinerMagnitude)))
+    print("Missing Liner Magnitude average of pixels: " + str(np.mean(missingLinerMagnitude)))
 
     temp = cv2.fastNlMeansDenoising(goodCapMagnitude, None, 15, 7, 21)
-    #cv2.imshow("temp", temp)
+    cv2.imshow("temp", temp)
 
-    #cv2.waitKey()
-    #cv2.destroyAllWindows()
+    cv2.waitKey()
+    cv2.destroyAllWindows()
 
     blobs = labelling.bestLabellingTestGradient(temp)
 
@@ -65,28 +134,37 @@ def test():
     #print (len(blobs))
 
     img = cv2.imread('./caps/g_04.bmp', cv2.IMREAD_COLOR)
-    #cv2.imshow('original', img)
-    #cv2.waitKey()
+    cv2.imshow('original', img)
+    cv2.waitKey()
 
     circles = []
 
     for blob in blobs:
         #if len(blob[0]) > 1 and checkCollinear(blob[0], blob[1]) == False:
         #if len(blob[0]) > 1:
-        #if len(blob[0]) > 2:
-        x, y, r, n = circledetection.leastSquaresCircleFitCached(blob[0], blob[1])
-            
-        if not math.isnan(x) or not math.isnan(y) or not math.isnan(r):
-            circles.append((x, y, r, n))
+        if len(blob[0]) > 2:
+            x, y, r, n = circledetection.leastSquaresCircleFitCached(blob[0], blob[1])
+            if not math.isnan(x) or not math.isnan(y) or not math.isnan(r):
+                circles.append((x, y, r, n))
 
     #print (len(circles))
     x, y, r = outliers.outliersElimination(circles, (100, 100))
     cv2.circle(img, (int(y), int(x)), int(r), (0, 255, 0), 1)
     cv2.circle(img, (int(y), int(x)), 2, (0, 0, 255), 3)
-    #cv2.imshow('circles', img)
-    #cv2.waitKey()
-    #cv2.destroyAllWindows()
+    cv2.imshow('circles', img)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    for i in range(0, 10):
-        test()
+    #for i in range(0, 10):
+        #test()
+
+    #for i in range(0, 20):
+    #    mask = circularmask(576, 768, center=None, radius=None)
+    #    pixelAverageMask(mask)
+
+    #    pixelAverage()
+
+    pixelAverage()
+    mask = circularmask(576, 768, center=None, radius=None)
+    pixelAverageMask(mask)
