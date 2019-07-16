c6fe16833d1119ca83aa2fb4eb6831b0f83aff62
Andrea Gavagna <andrea.gavagna@studio.unibo.it>
2019-07-16 17:09:14 +0200

    Refactor of test_outer_with_binarization: moved binarization instruction to a dedicated function in the module binarization.py binarization.py: now the binarization is done using otsu and closing. Using binary thresholding we can skip closing but we are less robust (still present but commented).

---
+++ binarization.py
@@ -0,0 +1,33 @@
+import numpy as np
+import cv2
+import math
+import os
+
+def binarize(img):
+    #hist = cv2.calcHist([img], [0], None, [256], [0,256])
+    #plt.plot(hist)
+    #plt.show()
+
+    #ret, thresh_binary = cv2.threshold(img, 10, 255, cv2.THRESH_BINARY)    #using this one instead of otsu we can skip closing, but we are less robust
+
+    ret, thresh_otsu = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    #print ('threshold found: ' + str(ret)) #print the threshold found with otsu (ret)
+                
+    #cv2.imshow('cap binary', thresh_binary)
+    #cv2.imshow('cap binary (otsu)', thresh_otsu)
+        
+    kernel = np.ones((7,7), np.uint8)
+    closing = cv2.morphologyEx(thresh_otsu, cv2.MORPH_CLOSE, kernel)
+
+    #kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7,7))    #using an elliptical kernel shape (obtain the same result)
+    #closing = cv2.morphologyEx(thresh_otsu, cv2.MORPH_CLOSE, kernel)
+
+    #cv2.imshow('closing', closing)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    return closing
+
+if __name__ == '__main__':
+    img = cv2.imread('caps/d_19.bmp', cv2.IMREAD_GRAYSCALE)
+    binarize(img)
--- tests.py
+++ tests.py
@@ -8,6 +8,7 @@ import labelling
 import circledetection
 import outliers
 import linerdefects_gradient
+import binarization
 
 def test_inner_circle():
     for file in os.listdir('./caps'):
@@ -119,32 +120,14 @@ def test_edge():
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-
 def test_outer_with_binarization():
     for file in os.listdir('./caps'):
         img = cv2.imread('caps/' + file, cv2.IMREAD_GRAYSCALE)
 
-        #hist = cv2.calcHist([img], [0], None, [256], [0,256])
-        #plt.plot(hist)
-        #plt.show()
-
-        ret, thresh1 = cv2.threshold(img, 10, 255, cv2.THRESH_BINARY)
-        #thresh2 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-        #ret2, thresh3 = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-
-        #print ('threshold found: ' + str(ret2))
-                
-        cv2.imshow('caps/' + file + ' circles standard', thresh1)
-        #cv2.imshow('caps/' + file + ' circles adaptive', thresh2)
-        #cv2.imshow('caps/' + file + ' circles otsu', thresh3)
-
-        edge_outer_circle = cv2.Canny(thresh1, 127, 254)
-
-        cv2.imshow('caps/' + file + ' circles edges', edge_outer_circle)
-
-        #circles = cv2.HoughCircles(thresh1, cv2.HOUGH_GRADIENT, 1, 100, param1=254, param2=20, minRadius=0, maxRadius=0)
-
-        blobs = labelling.bestLabellingTestGradient(thresh1)
+        binary = binarization.binarize(img)
+        #cv2.imshow('caps/' + file + ' binary', binary)
+        
+        blobs = labelling.bestLabellingTestGradient(binary)
 
         img = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
         circles = []
@@ -153,10 +136,7 @@ def test_outer_with_binarization():
             if len(blob[0]) > 2:
                 x, y, r, n = circledetection.leastSquaresCircleFitCached(blob[0], blob[1])
                 if not math.isnan(x) or not math.isnan(y) or not math.isnan(r):
-                    #if r > 200 and x > 0 and y > 0:
-                        circles.append((x, y, r, n))
-                        #cv2.circle(img, (int(y), int(x)), int(r), (0, 0, 255), 1)
-                        #cv2.circle(img, (int(y), int(x)), 2, (0, 255, 0), 1)
+                    circles.append((x, y, r, n))
 
         x, y, r = outliers.outliersElimination(circles, (20, 20))
         if not (x is None and y is None and r is None):
@@ -166,43 +146,32 @@ def test_outer_with_binarization():
             cv2.waitKey(0)
             cv2.destroyAllWindows()
 
-        #img = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
-        #circles = np.uint16(np.around(circles))
-        
-        #for circle in circles[0,:]:
-            # draw the outer circle
-            #cv2.circle(img, (round(circle[0]), round(circle[1])), round(circle[2]), (0,255,0), 2)
-            # draw the center of the circle
-            #cv2.circle(img, (round(circle[0]), round(circle[1])), 2, (0,0,255), 3)
-
-        #cv2.imshow('caps/' + file + ' circles', img)
-
-        #cv2.waitKey(0)
-        #cv2.destroyAllWindows()
-
 def test_missing_liner():
     for file in os.listdir('./caps'):
         img = cv2.imread('caps/' + file, cv2.IMREAD_GRAYSCALE)
         cv2.imshow('caps/' + file + ' circles', img)
 
-        ret, thresh1 = cv2.threshold(img, 10, 255, cv2.THRESH_BINARY)
+        binary = binarization.binarize(img)
+        #cv2.imshow('caps/' + file + ' binary', binary)
 
-        blobs = labelling.bestLabellingTestGradient(thresh1)
+        blobs = labelling.bestLabellingTestGradient(binary)
+
         circles = []
         for blob in blobs:
             if len(blob[0]) > 2:
                 x, y, r, n = circledetection.leastSquaresCircleFitCached(blob[0], blob[1])
                 if not math.isnan(x) or not math.isnan(y) or not math.isnan(r):
-                    if r > 200 and x > 0 and y > 0:
-                        circles.append((x, y, r, n))
+                   circles.append((x, y, r, n))
 
         x, y, r = outliers.outliersElimination(circles, (20, 20))
         if not (x is None and y is None and r is None):
             mask = linerdefects_gradient.circularmask(img.shape[0], img.shape[1], (x, y), r)
+            avg = np.mean(img[mask])
         else:
-            mask = linerdefects_gradient.circularmask(img.shape[0], img.shape[1], center=None, radius=None)
+            #mask = linerdefects_gradient.circularmask(img.shape[0], img.shape[1], center=None, radius=None)
+            avg = np.mean(img)
 
-        avg = np.mean(img[mask])
+        #avg = np.mean(img[mask])
         print("caps/" + file + " pixel's average: " + str(avg))
         if avg >= 60:
             print("caps/" + file + " has no liner!!")
@@ -217,4 +186,4 @@ if __name__ == '__main__':
     #test_outer_circle()
     #test_edge()
     test_outer_with_binarization()
-    test_missing_liner()
+    #test_missing_liner()
