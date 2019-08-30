4835e1d66f2ca0c7fedd907656ce1fe07b12bb74
dome <federicodomeniconi@gmail.com>
2019-08-30 13:33:19 +0200

    continuing refactoring

--- program.py
+++ program.py
@@ -5,8 +5,6 @@ import math
 import cv2
 import numpy as np
 
-def circle_detection(edges):
-
 
 def outer_circle_detection(img):
     # bynary mask + canny + labelling + least square + outliers (media o interpolazione finale?) (tests.py)
@@ -18,6 +16,23 @@ def outer_circle_detection(img):
 
     binary = binarization.binarize(img)
     mask = binary.copy().astype(bool)
+
+    # ----------
+    # if hough
+    # ----------
+
+    circles = cv2.HoughCircles(binary, cv2.HOUGH_GRADIENT, 1, 1, param1=200, param2=5, minRadius=0, maxRadius=0)
+    circles = np.uint16(np.around(circles))
+    
+    #   take only the center from the Hough result and compute the radius
+    x = sum([circles[0][i][1] for i in range(3)]) / 3
+    y = sum([circles[0][i][0] for i in range(3)]) / 3
+
+    # compute the radius as the mean distance between points and the center (found with Hough)
+    edges = cv2.Canny(binary, 100, 200, L2gradient=True)
+    pixels_x, pixels_y = np.nonzero(edges)
+
+    r = sum(np.sqrt((pixels_x - x)**2 + (pixels_y - y)**2)) / len(pixels_x)
 
     # ----------
     # if least square
@@ -39,33 +54,137 @@ def outer_circle_detection(img):
     blob_y = [y for circle in circles_remaining for y in circle[4][1]]
 
     x, y, r = circledetection.least_squares_circle_fit(blob_x, blob_y)
-    
-    # ----------
-    # if hough
-    # ----------
-
-    circles = cv2.HoughCircles(binary, cv2.HOUGH_GRADIENT, 1, 1, param1=200, param2=5, minRadius=0, maxRadius=0)
-    circles = np.uint16(np.around(circles))
-    
-    #   take only the center from the Hough result and compute the radius
-    x = sum([circles[0][i][1] for i in range(3)]) / 3
-    y = sum([circles[0][i][0] for i in range(3)]) / 3
-
-    # compute the radius as the mean distance between points and the center (found with Hough)
-    edges = cv2.Canny(binary, 100, 200, L2gradient=True)
-    pixels_x, pixels_y = np.nonzero(edges)
-
-    r = sum(np.sqrt((pixels_x - x)**2 + (pixels_y - y)**2)) / len(pixels_x)
 
     return x, y, r
 
 def inner_circle_detection(img):
-    # mask + stretching + mask + gaussian + canny + labelling + least square + outliers (media o interpolazione finale?) (tests.py)
-    # HOUGH EDGES
-    # mask + stretching + mask + gaussian + canny + labelling + blobs split + least square + bin outliers (test_with_bin_test.py)
-    # mask + stretching + mask + gaussian + canny + labelling + least square + outliers with remaining circles + iteration with cook least square (test2.py)
-    # HOUGH GAUSSIAN AND TAKE FIRST
-    # hough + 3 circles and mean of distance from canny    
+    # mask + stretching + mask + gaussian + (canny) + hough + best 1/2/3 for x,y (r) AND (mean for r)
+    # binary mask + hough + best 3 for x,y ---AND binary + canny + mean for r---
+
+    # mask + stretching + mask + gaussian + canny + labelling + (split) + least square + outliers mean/bin + mean/interpolation/cook on merged
+    # mask + stretching + mask + gaussian + canny + labelling + least square + cook on merged
+
+    x = None
+    y = None
+    r = None
+
+    # ----------
+    # if hough
+    # ----------
+
+    #
+    #
+    # COME CAZZO PRENDO IL RAGGIO GRANDE??????
+    #
+    #
+    # A SECONDA DELLE ESIGENZE POSSO AGGIUNGERE O NO I BLOB AL CERCHIO
+    # |-> BISOGNA SCRIVERE UN'ALTRA FUNZIONE APPOSTA DA CHIAMARE (outliers_elimination)
+    #
+    #
+    
+    binary = utils.binarize(img)
+    mask = binary.copy().astype(bool)
+
+    # method1
+    stretched = ((255 / (img[mask].max() - img[mask].min()))*(img.astype(np.float)-img[mask].min())).astype(np.uint8)
+    stretched[~mask] = 0
+    gaussian = cv2.GaussianBlur(stretched, (7,7), 2, 2)
+    edges = cv2.Canny(gaussian, 45, 100, apertureSize=3, L2gradient=True)
+
+    circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, 1, 1, param1=100, param2=10, minRadius=0, maxRadius=np.round(0.98*r_cap).astype("int"))
+
+    # method2
+    stretched = ((255 / (img[mask].max() - img[mask].min()))*(img.astype(np.float)-img[mask].min())).astype(np.uint8)
+    stretched[~mask] = 0
+    gaussian = cv2.GaussianBlur(stretched, (9,9), 2, 2)
+
+    circles = cv2.HoughCircles(gaussian, cv2.HOUGH_GRADIENT, 1, 1, param1=100, param2=10, minRadius=0, maxRadius=np.round(0.98*r_cap).astype("int"))
+
+    # method3
+
+    circles = cv2.HoughCircles(binary, cv2.HOUGH_GRADIENT, 1, 1, param1=200, param2=10, minRadius=0, maxRadius=0)
+
+    # all do best on 1/2/3
+
+    param = 2 # for example
+
+    x = sum(circles[0][i][1] for i in range(param)) / param
+    y = sum(circles[0][i][0] for i in range(param)) / param
+    r = sum(circles[0][i][2] for i in range(param)) / param
+
+    # ----------
+    # if our method
+    # ----------
+    binary = utils.binarize(img)
+    mask = binary.copy().astype(bool)
+    
+    stretched = ((255 / (img[mask].max() - img[mask].min()))*(img.astype(np.float)-img[mask].min())).astype(np.uint8)
+    stretched[~mask] = 0
+    gaussian = cv2.GaussianBlur(stretched, (7,7), 2, 2)
+    edges = cv2.Canny(gaussian, 45, 100, apertureSize=3, L2gradient=True)
+
+    blobs = utils.get_blobs(edges)
+
+    circles = []
+
+    # split?
+    for blob in blobs:
+        x_temp, y_temp, r_temp = circledetection.least_squares_circle_fit(blob[0], blob[1])
+        if not (math.isnan(x_temp) or math.isnan(y_temp) or math.isnan(r_temp)):
+            #if r < rCap - 5 and r > 150:
+            if r < 0.99*rCap:
+                circles.append((x_temp, y_temp, r_temp, len(blob[0])))
+
+    #else
+    # coloured_image = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    split_number = 8
+
+    for blob in blobs:
+        blob_x = blob[0]
+        blob_y = blob[1]
+        length = int(len(blob_x) / split_number) + 1
+        for i in range(split_number):
+        #     img_copy = coloured_image.copy()
+        #     color = (np.random.randint(0,255),np.random.randint(0,255),np.random.randint(0,255))
+        #     for j in range(0, length):
+        #         if j+i*length < len(blobX):
+        #             img_copy[blob_x[j+i*length]][blob_y[j+i*length]] = color
+
+            if length - 1 + i * length >= len(blob_x):
+                max_index = len(blob_x)-1
+            else:
+                max_index = length - 1 + i * length
+            x_temp, y_temp, r_temp = circledetection.least_squares_circle_fit(blob_x[i * length:max_index + 1], blob_y[i * length:max_index + 1])
+            if not (math.isnan(x_temp) or math.isnan(y_temp) or math.isnan(r_temp)):
+                if r < 0.99*rCap:
+                    circles.append((x_temp, y_temp, r_temp, len(blob_x[i * length:max_index]), (blob_x, blob_y)))
+
+    # outliers elimination with MEAN
+
+    # outliers elimintaion with BIN
+        # remaining circles with MEAN
+        # remaining circles MERGED
+            # interpolation with LEAST SQUARE
+            # interpolation with COOK
+    
+    # outliers MERGED
+        # interpolation with LEAST SQUARE
+        # interpolation with COOK
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return x, y, r
+
 
 def missing_liner_detection(img, threshold):
     # missing_liner_threshold = utils.get_missing_liner_threshold()
