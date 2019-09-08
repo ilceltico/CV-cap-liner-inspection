5e1e2ad72246cc56c27bea4202b8c34446dee777
Federico Stella <federico.stella4@studio.unibo.it>
2019-09-09 01:22:59 +0200

    Precise output of configuration errors

--- loadconfiguration.py
+++ loadconfiguration.py
@@ -2,6 +2,8 @@ import json
 
 HARALICK_THRESHOLD = 200
 
+ERROR_STRING = "Configuration error. See README.md to configure the software properly."
+
 OUTER_METHOD = ''
 OUTER_HOUGH_NUMBER_AVG = ''
 OUTER_RADIUS_COMPUTATION = ''
@@ -34,35 +36,48 @@ def parse_json():
         outer = config['outer']
         OUTER_METHOD = outer['method']
         if not OUTER_METHOD in ['hough', 'least_squares']:
-            print('Configuration error. See README.md to configure the software properly.')
+            print(ERROR_STRING)
+            print("circle_detection -> outer -> method")
             raise SystemExit(0)
 
         outer_parameters = outer['parameters'][OUTER_METHOD]
         if OUTER_METHOD == 'hough':
             OUTER_HOUGH_NUMBER_AVG = outer_parameters['average_best_circles']
             OUTER_RADIUS_COMPUTATION = outer_parameters['radius_computation']
-            if not isinstance(OUTER_HOUGH_NUMBER_AVG, int) or not OUTER_HOUGH_NUMBER_AVG > 0 or not OUTER_RADIUS_COMPUTATION in ['mean_radius', 'border_distance']:
-                print('Configuration error. See README.md to configure the software properly.')
+            if not isinstance(OUTER_HOUGH_NUMBER_AVG, int) or not OUTER_HOUGH_NUMBER_AVG > 0:
+                print(ERROR_STRING)
+                print("circle_detection -> outer -> parameters -> hough -> average_best_circles")
+                raise SystemExit(0)
+            if not OUTER_RADIUS_COMPUTATION in ['mean_radius', 'border_distance']:
+                print(ERROR_STRING)
+                print("circle_detection -> outer -> parameters -> hough -> radius_computation")
                 raise SystemExit(0)
         
         else:
             OUTER_LEAST_SQUARES_CIRCLE_GENERATION = outer_parameters['circle_generation']
             if not OUTER_LEAST_SQUARES_CIRCLE_GENERATION in ['mean', 'interpolation']:
-                print('Configuration error. See README.md to configure the software properly.')
+                print(ERROR_STRING)
+                print("circle_detection -> outer -> parameters -> least_squares -> circle_generation")
                 raise SystemExit(0)
 
         inner = config['inner']
         INNER_METHOD = inner['method']
         if not INNER_METHOD in ['hough', 'least_squares']:
-            print('Configuration error. See README.md to configure the software properly.')
-            raise SystemExit(0)
+                print(ERROR_STRING)
+                print("circle_detection -> inner -> method")
+                raise SystemExit(0)
 
         inner_parameters = inner['parameters'][INNER_METHOD]
         if INNER_METHOD == 'hough':
             INNER_CANNY_PRECISION = inner_parameters['canny_precision']
             INNER_HOUGH_NUMBER_AVG = inner_parameters['average_best_circles']
-            if not INNER_CANNY_PRECISION in ['precise', 'normal'] or not isinstance(INNER_HOUGH_NUMBER_AVG, int) or INNER_HOUGH_NUMBER_AVG < 1:
-                print('Configuration error. See README.md to configure the software properly.')
+            if not INNER_CANNY_PRECISION in ['precise', 'normal']:
+                print(ERROR_STRING)
+                print("circle_detection -> inner -> parameters -> hough -> canny_precision")
+                raise SystemExit(0)
+            if not isinstance(INNER_HOUGH_NUMBER_AVG, int) or INNER_HOUGH_NUMBER_AVG < 1:
+                print(ERROR_STRING)
+                print("circle_detection -> inner -> parameters -> hough -> average_best_circles")
                 raise SystemExit(0)
             
         else:
@@ -70,6 +85,19 @@ def parse_json():
             INNER_LEAST_SQUARES_MIN_BLOB_DIM = inner_parameters['min_blob_dim']
             INNER_LEAST_SQUARES_OUTLIERS_TYPE = inner_parameters['outliers_elimination_type']
             INNER_LEAST_SQUARES_CIRCLE_GENERATION = inner_parameters['circle_generation']
-            if not (isinstance(INNER_LEAST_SQUARES_SPLIT, bool)) or not (isinstance(INNER_LEAST_SQUARES_MIN_BLOB_DIM, int) and INNER_LEAST_SQUARES_MIN_BLOB_DIM > 0) or not (INNER_LEAST_SQUARES_OUTLIERS_TYPE in ['mean', 'votes']) or not (INNER_LEAST_SQUARES_CIRCLE_GENERATION in ['mean', 'interpolation', 'interpolation_cook']):
-                print('Configuration error. See README.md to configure the software properly.')
+            if not (isinstance(INNER_LEAST_SQUARES_SPLIT, bool)):
+                print(ERROR_STRING)
+                print("circle_detection -> inner -> parameters -> least_squares -> split_blobs")
+                raise SystemExit(0)
+            if not (isinstance(INNER_LEAST_SQUARES_MIN_BLOB_DIM, int) and INNER_LEAST_SQUARES_MIN_BLOB_DIM > 0):
+                print(ERROR_STRING)
+                print("circle_detection -> inner -> parameters -> least_squares -> min_blob_dim")
+                raise SystemExit(0)
+            if not (INNER_LEAST_SQUARES_OUTLIERS_TYPE in ['mean', 'votes']):
+                print(ERROR_STRING)
+                print("circle_detection -> inner -> parameters -> least_squares -> outliers_elimination_type")
+                raise SystemExit(0)
+            if not (INNER_LEAST_SQUARES_CIRCLE_GENERATION in ['mean', 'interpolation', 'interpolation_cook']):
+                print(ERROR_STRING)
+                print("circle_detection -> inner -> parameters -> least_squares -> circle_generation")
                 raise SystemExit(0)
